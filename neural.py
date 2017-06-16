e4356f4d1876e8b9ac46f46a9a348eb59931d8e6
Patrick <patrickprodrigues@hotmail.com>
2017-06-16 01:59:18 -0300

    Json save/load

--- neural.py
+++ neural.py
@@ -2,6 +2,7 @@
 #numpy - pacote para trabalhar com vetores e matrizes com várias dimensões.
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 
 #função que torna o sistema capaz de desviar da linearidade
 def nonlin(x,deriv=False):
@@ -9,7 +10,7 @@ def nonlin(x,deriv=False):
 	    return x*(1-x)
 
 	return 1/(1+np.exp(-x))
-    
+   
 def learn(X,Y):
     #seed para geração dos números aleatórios.    
     np.random.seed(1)
@@ -17,9 +18,10 @@ def learn(X,Y):
     #inicializa as sinapses com valores aleatórios.
     syn0 = 2 * np.random.random((3,4)) - 1
     syn1 = 2 * np.random.random((4,1)) - 1
+ 
     
     #loop de ajuste dos pesos.
-    for j in xrange(60000):
+    for j in xrange(10000):
     
     	 #Inicio da rede feed forwards
         l0 = X
@@ -45,7 +47,8 @@ def learn(X,Y):
         #ponto onde os valores das sinapses são atualizados
         syn1 += l1.T.dot(l2_delta)
         syn0 += l0.T.dot(l1_delta)
-    return syn0, syn1
+        
+    return syn0, syn1, l2_error
 
 #metodo de predição
 def prediction(inputs, layer0, layer1):
@@ -53,25 +56,51 @@ def prediction(inputs, layer0, layer1):
     r = nonlin(np.dot(l1,layer1))
     return r
 
+#carregado json com entradas da engine
+with open('gameInputs.json') as datafile:    
+    data = json.load(datafile)[0]
+    dataInputs = data['inputs']
+    dataOutputs = data['outputs']
+
 #entradas destinadas ao treinamento da rede
-x = np.array([[0,0,1],
-            [0,1,1],
-            [1,0,1],
-            [1,1,1]])
-#saidas destinadas ao treinamento                 
-y = np.array([[0],
-			[1],
-			[1],
-			[0]])
+#dataInput = [[0,0,1],[0,1,1],[1,0,1],[1,1,1]]
+x = np.array(dataInputs)
+
+#saidas destinadas ao treinamento 
+#dataOutput = [[0],[1],[1],[0]]
+y = np.array(dataOutputs)
   
 #treinamento, como resultado as sinapses com valores calibrados     
-layer0, layer1 = learn(x,y)
+layer0, layer1, error = learn(x,y)
 
 #predição de determinados valores   
-print prediction(x, layer0, layer1)
 
-plt.plot(prediction(x, layer0, layer1), 'ro')
+result = prediction(x, layer0, layer1)
+
+meanError = np.mean(np.abs(error))
+#erro medio de aprendizado 
+print "Erro médio de ", round((meanError * 100),2), "%"
+
+plt.plot(result, 'ro')
 plt.axis([-0.1, 3.1, -0.1 , 1.1])
 plt.grid(True)
 plt.show()
+
+jsonResults = [{'results':result.tolist(), 'error':meanError}]
+    
+with open('jsonResults.json', 'wb') as outfile:
+    json.dump(jsonResults, outfile)
+    
+    
+'''
+jsonData = [{'inputs':dataInputs, 'outputs':dataOutputs}]
+
+with open('jsonData.json', 'wb') as outfile:
+    json.dump(jsonData, outfile)
+  
+
+'''    
+
+
+
 
